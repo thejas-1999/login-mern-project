f39f1553fd7e26b43ec4d8c46f14f7c323dc4db5
thejas-1999 <thejask074@gmail.com>
2024-03-16 21:54:42 +0530

    complete the styles of username component

--- client/src/components/UserName.jsx
+++ client/src/components/UserName.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import avatar from "../assets/profile.png";
+import styles from "../styles/Username.module.css";
 
 const UserName = () => {
   return (
     <div className="container mx-auto">
-      <div className="flex justify-center items-start h-screen ">
-        <div>
+      <div className="flex justify-center items-center h-screen ">
+        <div className={styles.glass}>
           <div className="title flex flex-col items-center">
-            <h4 className="text-5xl font-bold">Hello Again!</h4>
+            <h4 className="text-5xl ">Hello Again!</h4>
             <span className="py-4 text-xl w-2/3 text-center text-gray-500">
               Explore more by connecting us.
             </span>
@@ -14,17 +16,23 @@ const UserName = () => {
 
           <form action="" className="py-1">
             <div className="profile flex justify-center py-4">
-              <img src="" alt="avatar" />
+              <img src={avatar} className={styles.profile_img} alt="avatar" />
             </div>
 
             <div className="textbook flex flex-col items-center gap-6">
-              <input type="text" placeholder="Username" />
-              <button type="submit">Let's Go</button>
+              <input
+                type="text"
+                placeholder="Username"
+                className={styles.textbox}
+              />
+              <button type="submit" className={`${styles.btn} bg-indigo-500`}>
+                Let's Go
+              </button>
             </div>
 
             <div className="text-center py-4">
               <span className="text-gray-500">
-                Not a Member{" "}
+                Not a Member
                 <Link to="/register" className="text-red-500">
                   Register Now
                 </Link>
