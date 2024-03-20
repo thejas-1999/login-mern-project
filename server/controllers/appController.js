38aae266f1fc451c4831892d4fd2daf0db74d7d2
thejas-1999 <thejask074@gmail.com>
2024-03-20 11:23:55 +0530

    create the basic controller for elements

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,1 +1,57 @@
+/** POST: http://localhost:8080/api/register 
+ * @param : {
+  "username" : "example123",
+  "password" : "admin123",
+  "email": "example@gmail.com",
+  "firstName" : "bill",
+  "lastName": "william",
+  "mobile": 8009860560,
+  "address" : "Apt. 556, Kulas Light, Gwenborough",
+  "profile": ""
+}
+*/
 
+export async function register(req, res) {
+  res.json(`register now `);
+}
+
+/** POST: http://localhost:8080/api/login 
+ * @param: {
+  "username" : "example123",
+  "password" : "admin123"
+}
+*/
+
+export async function login(req, res) {
+  res.json(`login route `);
+}
+
+/** GET: http://localhost:8080/api/user/example123 */
+export async function getUser(req, res) {
+  res.json(`getuser route `);
+}
+
+/** PUT: http://localhost:8080/api/updateuser */
+export async function updateUser(req, res) {
+  res.json(`updateuser route `);
+}
+
+/** GET: http://localhost:8080/api/generateOTP */
+export async function generateOTP(req, res) {
+  res.json(`generateOTP route `);
+}
+
+/** GET: http://localhost:8080/api/verifyOTP */
+export async function verifyOTP(req, res) {
+  res.json(`verifyOTP route `);
+}
+
+/** GET: http://localhost:8080/api/createResetSession */
+export async function createResetSession(req, res) {
+  res.json(`createResetSession route `);
+}
+
+/** PUT: http://localhost:8080/api/resetPassword */
+export async function resetPassword(req, res) {
+  res.json(`resetPassword route `);
+}
--- server/router/route.js
+++ server/router/route.js
@@ -1,24 +1,26 @@
 import { Router } from "express";
 
+//import all components
+
+import * as controller from "../controllers/appController.js";
+
 const router = Router();
 
 //POST Method
-router.route("/register").post((req, res) => {
-  res.json(`Register route`);
-});
+router.route("/register").post(controller.register); //register user
 
-router.route("/registerMail").post(); //register user
-router.route("/authendicate").post(); //authendicate user
-router.route("/login").post(); //login to the app
+/* router.route("/registerMail").post();  */ //send a mail
+router.route("/authendicate").post((req, res) => res.end()); //authendicate user
+router.route("/login").post(controller.login); //login to the app
 
 //GET Method
-router.route("/user/:username").get(); //user with username
-router.route("/generateOTP").get(); //generate random otp
-router.route("/verifyOTP").get(); //veryfy the otp
-router.route("/createResetSession").get(); //reset all the variables
+router.route("/user/:username").get(controller.getUser); //user with username
+router.route("/generateOTP").get(controller.generateOTP); //generate random otp
+router.route("/verifyOTP").get(controller.generateOTP); //veryfy the otp
+router.route("/createResetSession").get(controller.createResetSession); //reset all the variables
 
 //PUT Method
-router.route("/updateuser").put(); //update the user profile
-router.route("/resetPassword").put(); //use the reset password
+router.route("/updateuser").put(controller.updateUser); //update the user profile
+router.route("/resetPassword").put(controller.resetPassword); //use the reset password
 
 export default router;
